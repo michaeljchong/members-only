f585d1eb6983e2693ed479c16fc6ca63d7a810bc
Michael Chong <mikec0920@gmail.com>
2022-07-12 16:45:06 -0700

    Create posts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #get 'home/index'
+  get 'home/posts'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

450a3dc7f53bdfa14963a47ad125d98986ef4c23
Michael Chong <mikec0920@gmail.com>
2022-07-13 15:08:13 -0700

    Scaffold posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   #get 'home/index'
-  get 'home/posts'
+  get 'posts/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_02_015112) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_215556) do
+  create_table "posts", force: :cascade do |t|
+    t.string "subject"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

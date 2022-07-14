fb296b0d34f82a6eabba44c06172be48b4d818ca
Michael Chong <mikec0920@gmail.com>
2022-07-14 16:26:09 -0700

    Form relations between users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_205827) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_232353) do
   create_table "posts", force: :cascade do |t|
     t.string "subject"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,8 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_205827) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", default: "", null: false
-    t.string "username", default: "", null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
